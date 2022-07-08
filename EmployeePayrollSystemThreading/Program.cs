c378ab7af182a81500a40f01d1b071c156e9e4af
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-07-08 22:13:26 +0530

    [Tushar Vidwans] Adding welcome message

---
+++ EmployeePayrollSystemThreading/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayrollSystemThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll System -Threading Day 36");
+        }
+    }
+}
